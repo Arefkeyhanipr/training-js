124b0c434c51816d71ddd14ca8978edb56d55b05
Aref <Arefkeyhani.pr@gmail.com>
2024-03-30 02:22:17 +0330

    naming variables

---
+++ app.js
@@ -0,0 +1,13 @@
+//we can name variables in js 3 ways like this:
+
+//camel Case
+let firstName;
+let lastName;
+
+//snake_case
+let first_name;
+let last_name;
+
+//Pascal Case
+let FirstName;
+let LastName;
