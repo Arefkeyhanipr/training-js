cb05f9149a27336c9c053f1db8365d6cbb21efaf
Aref <Arefkeyhani.pr@gmail.com>
2024-03-30 02:36:10 +0330

    Atithmetic Operators

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
-//we can name variables in js 3 ways like this:
+// //we can name variables in js 3 ways like this:
 
-//camel Case
-let firstName;
-let lastName;
+// //camel Case
+// let firstName;
+// let lastName;
 
-//snake_case
-let first_name;
-let last_name;
+// //snake_case
+// let first_name;
+// let last_name;
 
-//Pascal Case
-let FirstName;
-let LastName;
+// //Pascal Case
+// let FirstName;
+// let LastName;
+
+// =============================================
+// =============================================
+
+//Atithmetic Operators
+
+const x = 150;
+const y = 300;
+
+const plus = x + y;
+console.log(`${x} plus ${y} will be ${plus}`);
+const minus = y - x;
+console.log(`${y} minus ${x} will be ${minus}`);
+const multi = x * y;
+console.log(`${x} multiply ${y} will be ${multi}`);
+const division = y / x;
+console.log(`Division of ${y} and ${x} will be ${division}`);
+const power = x ** y;
+console.log(`${x} power ${y} will be ${power}`);
+const leftOver = y % x;
+console.log(`leftOver of the division of ${y} & ${x} is ${leftOver}`);
