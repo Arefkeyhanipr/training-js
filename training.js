3a5680cc7636f6a7edbc3629d77beb70976b700c
Aref <Arefkeyhani.pr@gmail.com>
2024-03-30 03:30:20 +0330

    Checkout Order

---
+++ training.js
@@ -0,0 +1,36 @@
+// //------------------------------------------------     Simple Checkout Order Details     ------------------------------------------------
+
+// const shoe = 20;
+// const jeans = 15;
+// const tShirt = 5;
+
+// const arefBudget = 70;
+// const nickBudget = 40;
+// const allOrders = shoe + jeans + tShirt * 2;
+
+// const finalPrice = allOrders * 0.95;
+
+// console.log(`you need to pay ${allOrders}$ for this clothes`);
+
+// console.log(`the avarge price is : ${allOrders / 4}$ `);
+
+// console.log(
+//   `you got 0/5% discount with your promo code (${
+//     allOrders * 0.05
+//   }$ off) and now you can pay ${finalPrice}$`
+// );
+
+// const finalMassageForAref =
+//   arefBudget >= finalPrice
+//     ? `Aref You can buy all of your orders and have fun :)`
+//     : `Sorry Aref your budget is not enough you need more money`;
+
+// const finalMassageForNick =
+//   nickBudget >= finalPrice
+//     ? `Nick You can buy all of your orders and have fun :)`
+//     : `Sorry Nick your budget is not enough you need more money`;
+// console.log(finalMassageForAref);
+// console.log(finalMassageForNick);
+
+// =============================================
+// =============================================
