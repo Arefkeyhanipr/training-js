9ac1d19e151d676e9e459e8de68b58128b5a64e9
Aref <Arefkeyhani.pr@gmail.com>
2024-05-15 21:05:24 +0330

    Write a js code to print the patern given

--- training.js
+++ training.js
@@ -274,3 +274,22 @@
 // }
 
 // console.log(evenArray);
+
+// =============================================
+// =============================================
+//Write a js code to print bellow patern
+//// 1
+//// 1 2
+//// 1 2 3
+//// 1 2 3 4
+//// 1 2 3 4 5
+
+// const length = 5;
+
+// for (let i = 1; i <= length; i++) {
+//   let string = "";
+//   for (let j = 1; j <= i; j++) {
+//     string += j + " ";
+//   }
+//   console.log(string);
+// }
