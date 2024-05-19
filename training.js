905435eef258d24aa5392e4368cbe43037141926
Aref <Arefkeyhani.pr@gmail.com>
2024-05-19 18:46:37 +0330

    write a JS code to remove duplicate values

--- training.js
+++ training.js
@@ -293,3 +293,20 @@
 //   }
 //   console.log(string);
 // }
+
+// =============================================
+// =============================================
+
+//write a JS code to remove duplicate values
+
+// const array = [4, 2, 34, 14, 1, 12, 1, 4];
+
+// const newArray = [];
+
+// for (let i = 0; i < array.length; i++) {
+//   if (newArray.indexOf(array[i]) === -1) {
+//     newArray.push(array[i]);
+//   }
+// }
+
+// console.log(newArray);
