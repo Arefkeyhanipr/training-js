f8e23df2e15a9c9ba8e1a19253d0d72172370b15
Aref <Arefkeyhani.pr@gmail.com>
2024-05-21 23:33:21 +0330

    RPS miniproject, how alert and prompt work

---
+++ miniProjectRPS.js
@@ -0,0 +1,6 @@
+//Alert will give a pop up and says something,prompt will give an empty input so you can awnser
+// alert("Show");
+// console.log("rock");
+// const value = prompt();
+
+// console.log(value);
