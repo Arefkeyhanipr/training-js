00dc0ae5e67ffee3a89b7549bf634741a19ec334
Aref <Arefkeyhani.pr@gmail.com>
2024-05-21 23:55:49 +0330

    get the choice of player and show the computers choice

--- miniProjectRPS.js
+++ miniProjectRPS.js
@@ -1,6 +1,23 @@
 //Alert will give a pop up and says something,prompt will give an empty input so you can awnser
-// alert("Show");
-// console.log("rock");
+// alert("This is an alert");
 // const value = prompt();
-
 // console.log(value);
+
+const choices = ["rock", "paper", "scissors"];
+
+const playerChoice = prompt("choose rock,paper,scissors");
+
+if (
+  playerChoice === "rock" ||
+  playerChoice === "paper" ||
+  playerChoice === "scissors"
+) {
+  console.log(`You choose ${playerChoice}`);
+} else {
+  console.log(`You cheated !!! only use rock or paper or scissors !`);
+}
+
+const randomNum = Math.floor(Math.random() * choices.length);
+const computerChoice = choices[randomNum];
+
+console.log(`computer chooses ${computerChoice}`);
