1391d85d77c0631d8fbe2f036210d1bd418a4a89
Aref <Arefkeyhani.pr@gmail.com>
2024-05-22 19:08:26 +0330

    showing who win & who lose

--- miniProjectRPS.js
+++ miniProjectRPS.js
@@ -3,10 +3,13 @@
 // const value = prompt();
 // console.log(value);
 
+// we have only Rock & Paper & Scissors
 const choices = ["rock", "paper", "scissors"];
 
+// ask the player to choose RPS
 const playerChoice = prompt("choose rock,paper,scissors");
 
+//show the choice of the player
 if (
   playerChoice === "rock" ||
   playerChoice === "paper" ||
@@ -16,8 +19,33 @@ if (
 } else {
   console.log(`You cheated !!! only use rock or paper or scissors !`);
 }
-
+//computer needs to choose RPS
 const randomNum = Math.floor(Math.random() * choices.length);
 const computerChoice = choices[randomNum];
 
+//show the choice of computer
 console.log(`computer chooses ${computerChoice}`);
+
+//who win who lose
+
+if (playerChoice === computerChoice) {
+  console.log(`Its a tie`);
+} else if (playerChoice === "rock") {
+  if (computerChoice === "scissors") {
+    console.log(`Yeaaaah you WIN !!!`);
+  } else {
+    console.log(`Damnnn.sorry dude you lost ! :(`);
+  }
+} else if (playerChoice === "paper") {
+  if (computerChoice === "rock") {
+    console.log(`Yeaaaah you WIN !!!`);
+  } else {
+    console.log(`Damnnn.sorry dude you lost ! :(`);
+  }
+} else {
+  if (computerChoice === "paper") {
+    console.log(`Yeaaaah you WIN !!!`);
+  } else {
+    console.log(`Damnnn.sorry dude you lost ! :(`);
+  }
+}
