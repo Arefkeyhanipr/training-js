f4cc2bf913fdba01270d2be0a3a1aebfc874598c
Aref <Arefkeyhani.pr@gmail.com>
2024-06-01 20:55:30 +0330

    Validation of players entry

--- GTNminiproject.js
+++ GTNminiproject.js
@@ -1,11 +1,25 @@
 const randomNum = Math.ceil(Math.random() * 100);
 
+const validateNumber = (value) => {
+  if (isNaN(value)) {
+    return `You can only enter Numbers`;
+  } else if (+value < 1 || +value > 100) {
+    return `Enter a number between 1 and 100,Not more not less`;
+  }
+};
 console.log(randomNum);
 
 const play = () => {
   let num = prompt(`Choose a number between 1 & 100`);
-  if (num === null || num === "") {
-    console.log(`Enter a Number to play!`);
+  if (num === null) {
+    console.log(`You canceled the game!`);
+  } else if (num === "") {
+    console.log(`enter a number to play`);
+  }
+  const validation = validateNumber(num);
+  if (validation) {
+    console.log(validation);
+    return play();
   }
 };
 play();
