c16d68c2a89eee527cdcfe84f4392665446cc13c
Aref <Arefkeyhani.pr@gmail.com>
2024-06-01 22:52:40 +0330

    Limit the number of times player can play

--- GTNminiproject.js
+++ GTNminiproject.js
@@ -1,5 +1,5 @@
 const randomNum = Math.ceil(Math.random() * 100);
-
+let numberOfGuess = 0;
 const validateNumber = (value) => {
   if (isNaN(value)) {
     return `You can only enter Numbers`;
@@ -10,18 +10,30 @@ const validateNumber = (value) => {
 console.log(randomNum);
 
 const checkGuess = (guess) => {
+  numberOfGuess++;
+  console.log(`guess number ${numberOfGuess} was ${guess}`);
   if (guess === randomNum) {
     console.log(`You guessed right!`);
+    alert(`You guessed right!`);
+    document.write(`The Number was ${randomNum}`);
   } else if (guess > randomNum) {
     console.log(
       `Your guess (${guess}) was higher then The number, Try again! `
     );
+    play();
   } else {
     console.log(`Your guess (${guess}) was lower then The number, Try again! `);
+    play();
   }
 };
 
 const play = () => {
+  if (numberOfGuess === 10) {
+    console.log(`GAME OVER ! you ran out of guess`);
+    alert(`GAME OVER ! you ran out of guess`);
+    return;
+  }
+
   let num = prompt(`Choose a number between 1 & 100`);
   if (num === null) {
     console.log(`You canceled the game!`);
