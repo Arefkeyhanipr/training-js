3fc843b620e22ccbd3147cc96856fea5b586bf4f
Aref <Arefkeyhani.pr@gmail.com>
2024-06-11 21:07:35 +0330

    adding event to our filter btn's

--- productsProject/JS/app.js
+++ productsProject/JS/app.js
@@ -1,11 +1,15 @@
 const searchInput = document.getElementById("search-input");
 const products = document.querySelectorAll(".product-item");
+const buttons = document.querySelectorAll(".filter-btn");
+
+//Functions
 
 const searchHandeler = (event) => {
   const searchValue = event.target.value.toLowerCase().trim();
 
   products.forEach((product) => {
     const productName = product.children[1].innerText.toLowerCase();
+
     if (productName.includes(searchValue)) {
       product.style.display = "block";
     } else {
@@ -14,4 +18,16 @@ const searchHandeler = (event) => {
   });
 };
 
+const filterHandler = (event) => {
+  const filter = event.target.dataset.filter;
+};
+
+//Add event listener
+
 searchInput.addEventListener("keyup", searchHandeler);
+
+buttons.forEach((button) => {
+  button.addEventListener("click", filterHandler);
+});
+
+console.log(buttons);
