64c771d83e3776772b1d4a3f6b6cabfdcaa6bbc3
Aref <Arefkeyhani.pr@gmail.com>
2024-06-19 08:01:15 +0330

    module in js(secound way--> default)

--- Module/app.js
+++ Module/app.js
@@ -1,6 +1,6 @@
 //module is for using our codes in another js script
 //we can make big projects to small parts of js and not make a so big js file
-//first way---------- Named
+//first way---------- Named *********************
 
 const name = "Aref";
 const age = 23;
@@ -12,3 +12,12 @@ function hello() {
 //Use export to send the variables like the code below
 //we even can change the name of it with -as- like hello in here
 export { name, age, hello as greeting };
+
+//--------------- Secound way Default ***************
+
+//The differences between default module and named module is in default module we can only sent one thing to the other file
+//like the code below
+
+const city = "Tehran";
+
+export default city;
--- Module/script.js
+++ Module/script.js
@@ -1,4 +1,4 @@
-//first way---------- Named
+//first way---------- Named *****************
 
 //Use import and give the address to it so we can get the sent variables, like the code below
 import { name, age, greeting } from "./app.js";
@@ -6,3 +6,9 @@ import { name, age, greeting } from "./app.js";
 console.log(name);
 console.log(age);
 greeting();
+
+//--------------- Secound way Default ***************
+
+import city from "./app.js";
+
+console.log(city);
