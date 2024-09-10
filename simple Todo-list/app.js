9f1fb3355e5aeafeb956d70a28c7bb00d9ffdbe2
Aref <Arefkeyhani.pr@gmail.com>
2024-09-10 10:38:47 +0330

    deleting todo

--- simple Todo-list/app.js
+++ simple Todo-list/app.js
@@ -4,6 +4,7 @@ const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 //Event Listener
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
 //Functions
 function addTodo(e) {
   e.preventDefault();
@@ -30,4 +31,18 @@ function addTodo(e) {
   todoList.appendChild(todoDiv);
   //clear the input after adding the todo
   todoInput.value = "";
+}
+
+function deleteCheck(e) {
+  console.log(e.target.parentElement);
+  const item = e.target;
+  itemParent = item.parentElement;
+  //Delete todo
+  if (item.classList[0] === "trash-btn") {
+    itemParent.remove();
+  }
+  //Check Mark
+  if (item.classList[0] === "complete-btn") {
+    itemParent.classList.toggle("completed");
+  }
 }
